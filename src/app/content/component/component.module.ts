eee72ddb3cf41d9a0798c34159524f9970767fc7
jandrade-II <enggfailure@gmail.com>
2020-06-21 03:07:41 +0800

    update file structure

---
+++ src/app/content/component/component.module.ts
@@ -0,0 +1,31 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+  
+    
+  ],
+  entryComponents: [
+
+  ],
+  declarations: [ 
+
+  ],
+  exports: [
+
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ],
+  providers: [
+  
+  ]
+})
+export class ComponentModule {}
